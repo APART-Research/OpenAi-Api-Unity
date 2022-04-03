ede0ecd64c18b279662f361bd72ee261f78bd54c
dr-kranc <esben@eachthing.com>
2022-04-02 17:02:32 -0700

    Debug.Log

--- Runtime/Scripts/Json/Deserialization/JsonDeserializer.cs
+++ Runtime/Scripts/Json/Deserialization/JsonDeserializer.cs
@@ -9,7 +9,12 @@ namespace OpenAi.Json
         public static JsonObject FromJson(string json)
         {
             string[] tokens = JsonLexer.Lex(json);
+            Debug.Log(json);
             Debug.Log(tokens);
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                Debug.Log(tokens[i]);
+            }
             return JsonSyntaxAnalyzer.Parse(tokens);
         }
     }
